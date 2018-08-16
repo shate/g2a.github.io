3a8fdb3de1650e0e9221890c116f66f6e1a8b4a9
shate <shate@ya.ru>
2018-08-16 10:48:41 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('select').each(function(){
+	$(this).siblings('p').text( $(this).children('option:selected').text() );
+});
+$('select').change(function(){
+	$(this).siblings('p').text( $(this).children('option:selected').text() );
+});
+})
